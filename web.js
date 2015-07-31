a29d1b611325105b26c77bab1674054f47ed105b
Sam Clearman <sam@samclearman.com>
2015-07-31 14:21:38 -0400

    update packages and fix headers for .svgz files

--- web.js
+++ web.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var path = require("path");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
+function setHeaders(res,pth,stat) {
+    console.log(pth)
+    if (path.extname(pth) == ".svgz") {
+        console.log("setting gzip encoding")
+        res.setHeader("Content-Encoding", "gzip");
+    }
+}
 
-app.use("/", express.static('static'));
+app.use("/", express.static('static', {
+    "setHeaders": setHeaders,
+}));
 
 // app.get('/', function(req, res) {
 //   res.send('Hello World!');
