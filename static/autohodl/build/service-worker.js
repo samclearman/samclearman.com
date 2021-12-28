9f4fda6da17fb4ae895d70ce4c9fdfc7a5085c90
Sam Clearman <sam@scale.com>
2021-12-28 17:28:03 -0500

    update autohodl

--- static/autohodl/build/service-worker.js
+++ static/autohodl/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e52af9408dd38a7ca04b728170ee0b7.js"
+  "autohodl/precache-manifest.ae3848ff4c72aab862a287ecbf3d54e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("autohodl/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
